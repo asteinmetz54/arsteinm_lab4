bc6700b8cc5986c32325f6f3a763011c2aadb4c0
Andrew Steinmetz <asteinmetz54@gmail.com>
2016-11-13 19:55:37 -0800

    added delete functionality for admin

--- README.txt
+++ README.txt
@@ -1,11 +1,15 @@
 SER421 Lab4
-Jared Huber <jwhuber>
 Andrew Steinmeetz <arsteinm>
 
 README
 
+all work since previous upload was my own work since i used my latepass to finish functionality of this lab
+
 to access admin username and password are admin
 
 answers will be prepopulated off users stored in userstore.json
-test username and password for andrew, enter in direct url for /survey/1 to test
-the get of /matches got broken, but if reached by post from 4th survey question is functional
+
+test username and password for andrew for one of the users in the database
+
+delete functionality for admin is found once clicked on a user's info
+
--- server.js
+++ server.js
@@ -61,97 +61,115 @@ app.post('/process_post', urlencodedParser, function (req, res) {
 			res.redirect('/survey/1');
 		}
 	} else {
-		res.setHeader(403);
-		res.send("Incorrect username or password. Go back an try again.");
+		res.status(401).send("Incorrect username or password. Go back an try again.");
 	}
 });
 
 
 //survey question 1
 app.all('/survey/1', function (req, res, next) {
-	var temp = readJson('survey.json');
-
-	app.locals.questionNum = 'Question 1';
-	app.locals.question = temp[0].question;
-	app.locals.nextQuestion = "/survey/2";
-	app.locals.inputName = "name";
-
-	//check if existing user to populate answer
-	if (userExist(req.session.user)) {
-		app.locals.previousAnswer = loadUserAnswer(req.session.user, 1);
+	if (!req.session.user) {
+		res.status(401).send("Not logged in, please return to login screen to login");
 	} else {
-		app.locals.previousAnswer = "";
+		var temp = readJson('survey.json');
+
+		app.locals.questionNum = 'Question 1';
+		app.locals.question = temp[0].question;
+		app.locals.nextQuestion = "/survey/2";
+		app.locals.inputName = "name";
+
+		//check if existing user to populate answer
+		if (userExist(req.session.user)) {
+			app.locals.previousAnswer = loadUserAnswer(req.session.user, 1);
+		} else {
+			app.locals.previousAnswer = "";
+		}
+		res.render('main_jade');
+		next();
 	}
-	res.render('main_jade');
-	next();
 });
 
 //survey question 2
 app.all('/survey/2', urlencodedParser, function (req, res, next) {
-	var temp = readJson('survey.json');
-	var name;
-	app.locals.questionNum = 'Question 2';
-	app.locals.question = temp[1].question;
-	app.locals.nextQuestion = "/survey/3";
-	app.locals.inputName = "quest";
-
-	if (userExist(req.session.user)) {
-		app.locals.previousAnswer = loadUserAnswer(req.session.user, 2);
+	if (!req.session.user) {
+		res.status(401).send("Not logged in, please return to login screen to login");
 	} else {
-		app.locals.previousAnswer = "";
+		var temp = readJson('survey.json');
+		var name;
+		app.locals.questionNum = 'Question 2';
+		app.locals.question = temp[1].question;
+		app.locals.nextQuestion = "/survey/3";
+		app.locals.inputName = "quest";
+
+		if (userExist(req.session.user)) {
+			app.locals.previousAnswer = loadUserAnswer(req.session.user, 2);
+		} else {
+			app.locals.previousAnswer = "";
+		}
+		if (req.method == "POST") {
+			req.session.name = req.body.name;
+		}
+		res.render('main_jade');
+		next();
 	}
-	if (req.method == "POST") {
-		req.session.name = req.body.name;
-	}
-	res.render('main_jade');
-	next();
 });
 
 
 //survey question 3
 app.all('/survey/3', urlencodedParser, function (req, res, next) {
-	var temp = readJson('survey.json');
-
-	app.locals.questionNum = 'Question 3';
-	app.locals.question = temp[2].question;
-	app.locals.nextQuestion = "/survey/4";
-	app.locals.inputName = "color";
-
-	if (userExist(req.session.user)) {
-		app.locals.previousAnswer = loadUserAnswer(req.session.user, 3);
+	if (!req.session.user) {
+		res.status(401).send("Not logged in, please return to login screen to login");
 	} else {
-		app.locals.previousAnswer = "";
+		var temp = readJson('survey.json');
+
+		app.locals.questionNum = 'Question 3';
+		app.locals.question = temp[2].question;
+		app.locals.nextQuestion = "/survey/4";
+		app.locals.inputName = "color";
+
+		if (userExist(req.session.user)) {
+			app.locals.previousAnswer = loadUserAnswer(req.session.user, 3);
+		} else {
+			app.locals.previousAnswer = "";
+		}
+		if (req.method == "POST") {
+			req.session.quest = req.body.quest;
+		}
+		res.render('main_jade');
+		next();
 	}
-	if (req.method == "POST") {
-		req.session.quest = req.body.quest;
-	}
-	res.render('main_jade');
-	next();
+
 });
 
 
 //survey question 4
 app.all('/survey/4', urlencodedParser, function (req, res, next) {
-	var temp = readJson('survey.json');
-
-	app.locals.questionNum = 'Question 4';
-	app.locals.question = temp[3].question;
-	app.locals.nextQuestion = "/survey/process";
-	app.locals.inputName = "capital";
-
-	if (userExist(req.session.user)) {
-		app.locals.previousAnswer = loadUserAnswer(req.session.user, 4);
+	if (!req.session.user) {
+		res.status(401).send("Not logged in, please return to login screen to login");
 	} else {
-		app.locals.previousAnswer = "";
+		var temp = readJson('survey.json');
+
+		app.locals.questionNum = 'Question 4';
+		app.locals.question = temp[3].question;
+		app.locals.nextQuestion = "/survey/process";
+		app.locals.inputName = "capital";
+
+		if (userExist(req.session.user)) {
+			app.locals.previousAnswer = loadUserAnswer(req.session.user, 4);
+		} else {
+			app.locals.previousAnswer = "";
+		}
+		if (req.method == "POST") {
+			req.session.color = req.body.color;
+		}
+		res.render('main_jade');
+		next();
 	}
-	if (req.method == "POST") {
-		req.session.color = req.body.color;
-	}
-	res.render('main_jade');
-	next();
+
 });
 
 app.post("/survey/process", urlencodedParser, function (req, res, next) {
+
 	var store = readJson('userstore.json');
 	req.session.capital = req.body.capital;
 	var answers = [req.session.name, req.session.quest, req.session.color, req.session.capital];
@@ -171,44 +189,47 @@ app.post("/survey/process", urlencodedParser, function (req, res, next) {
 });
 
 app.all('/matches', urlencodedParser, function (req, res, next) {
-	//endpoint that renders the best partner matches based on survey results
-	var store = readJson('userstore.json');
-	var username = req.cookies.username;
-	userIndex = getUserIndex(username);
+	if (!req.session.user) {
+		res.status(401).send("Not logged in, please return to login screen to login");
+	} else {
+		//endpoint that renders the best partner matches based on survey results
+		var store = readJson('userstore.json');
+		var username = req.cookies.username;
+		userIndex = getUserIndex(username);
 
-	if (req.method == "POST") {
-		user = [req.session.name, req.session.quest, req.session.color, req.session.capital];
-	} else if (req.method == "GET") {
-		user = [store[userIndex].answer[0], store[userIndex].answer[1], store[userIndex].answer[2], store[userIndex].answer[3]];
-	}
-
-	//compare users answers to userstore
-	var match = new Array();
-
-	//outer loop to cycle through userstore
-	for (var i = 0; i < store.length; i++) {
-		var count = 0;
-		//make sure not user currently logged in that you are comparing
-		if (i != userIndex) {
-			//inner loop to cycle through user answers
-			for (var j = 0; j < 4; j++) {
-				if (removePunct(store[i].answer[j]).toLowerCase() == removePunct(user[j]).toLowerCase()){
-					count++;
-				}
-			}
-			match.push({"user": store[i].user, "matches": count});
+		if (req.method == "POST") {
+			user = [req.session.name, req.session.quest, req.session.color, req.session.capital];
+		} else if (req.method == "GET") {
+			user = [store[userIndex].answer[0], store[userIndex].answer[1], store[userIndex].answer[2], store[userIndex].answer[3]];
 		}
-	}
-	match = bubbleSort(match);
-	console.log("after bubblesort: " + JSON.stringify(match));
-	var sortMatches = [];
-	for (var i = 0; i < match.length; i++){
-		sortMatches[i] = match[i].user;
-	}
 
-	app.locals.matches = sortMatches;
-	res.render('matches_jade');
-	res.end();
+		//compare users answers to userstore
+		var match = new Array();
+
+		//outer loop to cycle through userstore
+		for (var i = 0; i < store.length; i++) {
+			var count = 0;
+			//make sure not user currently logged in that you are comparing
+			if (i != userIndex) {
+				//inner loop to cycle through user answers
+				for (var j = 0; j < 4; j++) {
+					if (removePunct(store[i].answer[j]).toLowerCase() == removePunct(user[j]).toLowerCase()) {
+						count++;
+					}
+				}
+				match.push({ "user": store[i].user, "matches": count });
+			}
+		}
+		match = bubbleSort(match);
+		var sortMatches = [];
+		for (var i = 0; i < match.length; i++) {
+			sortMatches[i] = match[i].user;
+		}
+
+		app.locals.matches = sortMatches;
+		res.render('matches_jade');
+		res.end();
+	}
 });
 
 
@@ -229,51 +250,65 @@ app.get('/logout', function (req, res) {
 app.get('/tools', function (req, res) {
 	//endpoint for the admin functionality (6a) and should only be available to the admin.
 	if (req.session.user !== 'admin') {
-		res.status(401);
-		res.send("Authorization required. Please try again.");
-		res.redirect('/');
+		res.status(401).send("Admin authorization required. Please try again.");
+	} else {
+		store = readJson('userstore.json');
+		total = store.length;
+		app.locals.totalSurveys = total;
+
+		var users = [];
+		for (var i = 0; i < store.length; i++) {
+			users[i] = store[i].user;
+		}
+		app.locals.Users = users;
+
+		res.render('tools_jade');
 	}
 
-	store = readJson('userstore.json');
-	total = store.length;
-	app.locals.totalSurveys = total;
 
-	res.render('tools_jade');
 });
 
 app.get('/user/:userId', function (req, res) {
-	var temp = readJson('survey.json');
-	var result = new Array();
-	var name = req.params.userId;
-	name = name.substr(1);
+	//endpoint for the admin functionality (6a) and should only be available to the admin.
+	if (req.session.user !== 'admin') {
+		res.status(401).send("Admin authorization required. Please try again.");
+	} else {
+		var store = readJson('userstore.json');
+		var result = new Array();
+		var name = req.params.userId;
+		name = name.substr(1);
+		var index = getUserIndex(name);
+		app.locals.username = name;
+		app.locals.name = store[index].answer[0];
+		app.locals.quest = store[index].answer[1];
+		app.locals.color = store[index].answer[2];
+		app.locals.capital = store[index].answer[3];
 
-	for (var i = 0; i < temp[0].answer.length; i++) {
-		if (name == temp[0].answer[i]) {
-			app.locals.user = name;
-			app.locals.quest = temp[1].answer[i];
-			app.locals.color = temp[2].answer[i];
-			app.locals.capital = temp[3].answer[i];
-		}
+
+		res.render('user_jade');
 	}
 
-	res.render('user_jade');
 })
 
 app.get('/deleteEntry/:userId', function (req, res) {
-	var temp = readJson('survey.json');
-	var result = new Array();
-	var name = req.params.userId;
-	name = name.substr(1);
-
-	for (var i = 0; i < temp[0].answer.length; i++) {
-		if (name == temp[0].answer[i]) {
-			temp.splice(temp[0].answer[i]);
-			temp.splice(temp[1].answer[i]);
-			temp.splice(temp[2].answer[i]);
-			temp.splice(temp[3].answer[i]);
-		}
+	//endpoint for the admin functionality (6a) and should only be available to the admin.
+	if (req.session.user !== 'admin') {
+		res.status(401).send("Admin authorization required. Please try again.");
+	} else {
+		var store = readJson('userstore.json');
+		var name = req.params.userId;
+		name = name.substr(1);
+		console.log("name is:" + name +";");
+		var index = getUserIndex(name);
+		console.log("index of user is: " + index);
+		
+		delete store[index];
+		store.splice(index, 1);
+		console.log(JSON.stringify(store));
+		
+		writeJson(store);
+		res.redirect('/tools');
 	}
-	writeJson(temp);
 })
 
 
